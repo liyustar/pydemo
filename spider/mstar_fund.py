cd74a8c5b3f524ffdaaa770c533109c2283e1fef
liyustar <liyustar@gmail.com>
2017-07-19 00:09:34 +0800

    add mstar_fund.py

---
+++ spider/mstar_fund.py
@@ -0,0 +1,49 @@
+# coding: utf-8
+
+from spider import util, mysqlutil
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+# 基金分类
+category = [
+    "stock",    # 股票型
+    "sector_stock-medicine",    # 行业股票 - 医药
+    "sector_stock-tech",    # 行业股票 - 科技、传媒及通讯
+    "sh_hk_sz_stock",    # 沪港深股票型基金
+    "mix_radical",    # 激进配置型
+    "mix_flexible",    # 灵活配置型
+    "mix_standard",    # 标准混合型
+    "mix_keep",    # 保守混合型
+    "mix_sh_hk_sz",    # 沪港深混合型基金
+    "bond_convertible",    # 可转债
+    "bond_radical",    # 激进债券型
+    "bond_general",    # 普通债券型
+    "bond_pure",    # 纯债基金
+    "bond_short",    # 短债基金
+    "currency",    # 货币市场基金
+    "market_neutral",    # 市场中性策略
+    "commodities",    # 商品
+    "keep",    # 保本基金
+    "other",    # 其它
+]
+
+# 请求基金信息的URL
+temp_utl = 'http://morningstar.cn/handler/fundranking.ashx?' \
+           'date=2017-07-14' \
+           '&fund=' \
+           '&category=stock' \
+           '&rating=' \
+           '&company=' \
+           '&cust=' \
+           '&sort=ReturnYTD' \
+           '&direction=desc' \
+           '&tabindex=0' \
+           '&pageindex=1' \
+           '&pagesize=10000' \
+           '&randomid=0.28046837032975325'
+
+
+if __name__ == '__main__':
+    # url = 'https://cn.morningstar.com/fundcompany/default.aspx'
+    content = util.getHtml(temp_utl)
+    util.saveHtml('mstar_fund.html', content)
